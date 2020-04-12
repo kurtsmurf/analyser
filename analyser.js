0a1f54d4edf58eb382a7ef41c14d8d1c115d0493
Chris Smith <chrisronsmith@gmail.com>
2020-04-12 14:17:03 -0400

    Make it kind of 3d

--- analyser.js
+++ analyser.js
@@ -1,69 +1,74 @@
-const ac = new AudioContext()
-const b = ac.createBufferSource()
-const a = ac.createAnalyser()
+const audioContext = new AudioContext()
+const bufSrc = audioContext.createBufferSource()
+const analyser = audioContext.createAnalyser()
+analyser.fftSize = 64
 
-b.connect(a).connect(ac.destination)
-
-const c = document.getElementById('analyser')
-const r = c.getContext('2d')
-
-const binsPerRow = 32
-const rowCount = 8
-const scale = 10
-
-c.width = binsPerRow * scale
-c.height = 32 * scale
-
-r.scale(scale,scale)
-
-let bins = new Uint8Array(binsPerRow * rowCount)
-
-const getBins = () => {
-  const binWidth = a.frequencyBinCount / binsPerRow
-  const data = new Uint8Array(a.frequencyBinCount)
-
-  a.getByteFrequencyData(data)
-
-  bins.forEach((_, i) => {
-    bins[i] = Math.floor(data[binWidth * i] / 8)
-  })
-
-  return bins
-}
-
-let frame
-
-const draw = () => {
-  const w = c.width / scale
-  const h = c.height / scale
-
-  r.fillStyle = 'rgba(255,255,255,.2)'
-  r.fillRect(0,0,w,h)
-
-  getBins().forEach((v, i) => {
-    r.fillStyle = `hsl(${v * 18 + 15},100%, 50%)`
-
-    const x = i
-    const y = h - v
-
-    r.fillRect(x,y,1,1)
-  })
-
-  frame = requestAnimationFrame(draw)
-}
+const canvas = document.getElementById('analyser')
+const renderCtx = canvas.getContext('2d')
+const scale = 8
+const numRows = 64
+canvas.width = analyser.frequencyBinCount * 3 * scale
+canvas.height = (numRows + 32) * scale
+renderCtx.scale(scale,scale)
 
 
 fetch('mk_drmz.wav')
 .then(response => response.arrayBuffer())
-.then(arrayBuffer => ac.decodeAudioData(arrayBuffer))
+.then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
 .then(audioBuffer => {
-  b.buffer = audioBuffer
-  b.loop = true
-  b.start()
-  draw()
-
-  setTimeout(() => {
-    b.stop()
-    cancelAnimationFrame(frame)
-  }, 1000)
+  bufSrc.buffer = audioBuffer
+  bufSrc.loop = true
+  bufSrc.connect(analyser)//.connect(audioContext.destination)
+  bufSrc.start()
 })
+
+// state
+const rows = [...Array(numRows)].map(_ => new Uint8Array(analyser.frequencyBinCount))
+
+// input
+const getFreqData = () => {
+  const freqData = new Uint8Array(analyser.frequencyBinCount)
+  analyser.getByteFrequencyData(freqData)
+
+  return freqData
+}
+
+const updateRows = () => {
+  rows.unshift(scaleBins(getFreqData()))
+  rows.pop()
+}
+
+// pure
+const scaleBins = (bins) => {
+  return bins.map(binVal => binVal * (32/256))
+}
+
+// output
+let frame
+
+const drawRow = (rowValues, rowIndex) => {
+  const hue = rowIndex * 4
+  renderCtx.fillStyle = `hsl(${hue},100%,50%)`
+
+  const h = canvas.height / scale
+  rowValues.forEach((v,i) => {
+    x = i + rowIndex
+    y = h - v - rowIndex
+
+    renderCtx.fillRect(x,y,1,1)
+  })
+}
+
+const render = () => {
+  renderCtx.clearRect(0,0,999999,9999999)
+  rows.forEach(drawRow)
+}
+
+// act
+animate = () => {
+  updateRows()
+  render()
+
+  frame = requestAnimationFrame(animate)
+}
+animate()
