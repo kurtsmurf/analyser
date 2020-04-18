ccf18f21424d8200199c7b9e6e9354100657e06e
Chris Smith <chrisronsmith@gmail.com>
2020-04-18 13:00:53 -0400

    Move mountain to Muhammed
    Translate rendering context before rendering each row to simplify calculation of coordinates.

--- analyser.js
+++ analyser.js
@@ -8,14 +8,14 @@ const renderCtx = canvas.getContext('2d')
 const scale = 16
 const numRows = 32
 const depth = 32
-canvas.width = (analyser.frequencyBinCount + numRows) * scale
-canvas.height = (numRows + depth) * scale
+canvas.width = (analyser.frequencyBinCount + numRows - 1) * scale
+canvas.height = (numRows + depth - 1) * scale
 renderCtx.lineCap = 'round'
 renderCtx.lineJoin = 'round'
 renderCtx.scale(scale,scale)
 
 // state
-const rows = [...Array(numRows)].map(_ => new Uint8Array(analyser.frequencyBinCount))
+const freqDataRows = [...Array(numRows)].map(_ => new Uint8Array(analyser.frequencyBinCount))
 
 // input
 const getFreqData = () => {
@@ -26,8 +26,8 @@ const getFreqData = () => {
 }
 
 const updateRows = () => {
-  rows.unshift(scaleBins(getFreqData()))
-  rows.pop()
+  freqDataRows.unshift(scaleBins(getFreqData()))
+  freqDataRows.pop()
 }
 
 // pure
@@ -49,46 +49,58 @@ let frame = 0
 
 
 
-const drawRow = (rowValues, rowIndex) => {
-  const h = canvas.height / scale
-
+const drawFreqData = (freqData, lineWidth, saturation) => {
   // create path object from frequency data
   const path = new Path2D()
-  path.moveTo(rowIndex + 1, h - rowValues[0] - 1 - rowIndex)
+  path.moveTo(0, depth - freqData[0])
 
-  for (let i = 1; i<rowValues.length;i++) {
-    const v = rowValues[i]
-    const x = i + 1 + rowIndex
-    const y = h - v - 1 - rowIndex
+  for (let i = 1; i<freqData.length;i++) {
+    const x = i
+    const y = depth - freqData[i]
 
     path.lineTo(x,y)
   }
 
-  // calculate hue and saturation
+  // let overall hue drift over time
   const baseHue = (frame % 1800) / 5
-  const saturation = 125 - (rowIndex / numRows) * 100
-
-  // calculate upper/lower bounds of row box
-  const bottom = h - rowIndex
-  const top = bottom - depth
 
   // draw wider line
-  renderCtx.strokeStyle = verticalGradient(bottom, top, baseHue, saturation, 85)
-  renderCtx.lineWidth = 2
+  renderCtx.strokeStyle = verticalGradient(depth, 0, baseHue, saturation, 85)
+  renderCtx.lineWidth = lineWidth
   renderCtx.stroke(path)
 
   // draw narrower line
-  renderCtx.strokeStyle = verticalGradient(bottom, top, baseHue, saturation, 50)
-  renderCtx.lineWidth = 1.5
+  renderCtx.strokeStyle = verticalGradient(depth, 0, baseHue, saturation, 50)
+  renderCtx.lineWidth = lineWidth * .75
   renderCtx.stroke(path)
+
+  // temp - draw guide
+  renderCtx.strokeStyle = 'white'
+  renderCtx.lineWidth = .125
+  renderCtx.strokeRect(0,1,freqData.length - 1,depth - 1)
 }
 
 const drawFrame = () => {
+  // save default transform
+  const defaultTransform = renderCtx.getTransform()
+  
   renderCtx.clearRect(0,0,canvas.width,canvas.height)
 
-  for (let i = rows.length; i>0;i--) {
-    drawRow(rows[i - 1], i - 1)
+  // move origin to initial position
+  renderCtx.translate(numRows, -1)
+
+  // draw data rows back to front
+  for (let i = numRows - 1; i > 0; i--) {
+    // update origin
+    renderCtx.translate(-1, 1)
+
+    const saturation = 125 - (i / numRows) * 100
+
+    drawFreqData(freqDataRows[i], 2, saturation)
   }
+
+  // restore default transform
+  renderCtx.setTransform(defaultTransform)
 }
 
 const animate = () => {
